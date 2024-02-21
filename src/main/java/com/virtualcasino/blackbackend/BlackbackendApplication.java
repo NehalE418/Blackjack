864e67f711063b9d72966fe3022522e0ce2447e3
NehalE418 <hossain.nehal121998@gmail.com>
2024-02-21 23:12:06 +0530

    First commit 0.0.1

---
+++ src/main/java/com/virtualcasino/blackbackend/BlackbackendApplication.java
@@ -0,0 +1,13 @@
+package com.virtualcasino.blackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackbackendApplication.class, args);
+	}
+
+}
